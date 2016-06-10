9f9851350d697cef226d5d4c1424d4ab1eb09825
Богдан Бреус <muller.09@mail.ru>
2016-06-10 09:14:51 +0300

    Update zavd1.c

--- zavd1.c
+++ zavd1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
+double Geron(double a, double b, double c){
+	
+	double S, p;
+	
+	p = (a + b + c) / 2;
+	
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
+	
+	return S;
+}
+
 int main(){
 	
 	float a, b, c, p, S;
@@ -14,11 +25,7 @@ int main(){
 	printf("\nVvedite dovjiny b storony: ");
 	scanf("%f", &c);
 	
-	p = (a + b + c) / 2;
-	
-	S = sqrt(p * (p - a) * (p - b) * (p - c));
-	
-	printf("\nPlosha S = %.2f", S);
+	printf("\nPlosha S = %.2f", Geron(a, b, c));
 
 	return 0;
 }
